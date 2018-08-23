bade136b6c54e4b8e1eff9a06148a91868cdb23a
Alexander Demianenko <ale.de635@gmail.com>
2018-08-23 05:11:30 +0300

    initial commit

---
+++ MultiTracker/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] {"value123", "value223"};
+        }
+    }
+}
